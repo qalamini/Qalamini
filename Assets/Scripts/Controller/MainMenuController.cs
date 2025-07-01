097955a994e9dc8e87c1505812ecd163ef271f65
Syuhada Rantisi <suadarantisi@gmail.com>
2025-07-02 00:07:49 +0700

    Add VolumeManager with AudioMixer

--- Assets/Scripts/Controller/MainMenuController.cs
+++ Assets/Scripts/Controller/MainMenuController.cs
@@ -5,19 +5,18 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private Button gameButton;
-    [SerializeField] private Button quitButton;
+    [SerializeField] private Button settingsButton;
     [SerializeField] private Button profileButton;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private GameObject settingsUI;
 
     void Start()
     {
+        settingsUI.SetActive(false);
         gameButton.onClick.AddListener(() => SceneManager.LoadScene("LevelMenu"));
         profileButton.onClick.AddListener(() => SceneManager.LoadScene("Profile"));
-        quitButton.onClick.AddListener(QuitGame);
+        settingsButton.onClick.AddListener(() => settingsUI.SetActive(true));
+        closeButton.onClick.AddListener(() => settingsUI.SetActive(false));
     }
 
-    private void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Quit Game Called"); // Tambahkan debug log untuk memastikan di Editor
-    }
 }
